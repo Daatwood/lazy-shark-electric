aff2e8a554ecbbf6e08ea7448b20d00f1738fe01
Dustin Atwood <dustin@atwood.email>
2017-06-26 19:54:05 -0500

    production asset fix

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,11 +1,5 @@
 class StaticController < ApplicationController
   def index
-    @background_images = []
-    9.times do |i|
-      @background_images << "background#{i+1}.jpg"
-    end
-    puts "#{@background_images.inspect}"
-    @images = @background_images.sample(3)
     @color = 'blue'
   end
 end
