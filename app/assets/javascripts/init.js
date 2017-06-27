b330ef26e1f77e605ace5b896a853e0ccebd7dab
Dustin Atwood <dustin@atwood.email>
2017-06-26 20:34:11 -0500

    removed thingy

--- app/assets/javascripts/init.js
+++ app/assets/javascripts/init.js
@@ -2,9 +2,9 @@ var ready = function() {
   $('.button-collapse').sideNav();
   $('.parallax').parallax();
   $('.carousel').carousel();
-  setTimeout(function(){
-    $('.tap-target').tapTarget('open');
-  }, 2500);
+  // setTimeout(function(){
+  //   $('.tap-target').tapTarget('open');
+  // }, 2500);
 };
 
 $(document).ready(ready);
