e051c7f93dfa87c11af24bbd52d9bde111665a4e
Dustin Atwood <dustin@atwood.email>
2017-08-17 13:58:51 -0500

    removed commented code and unused images

--- app/assets/javascripts/init.js
+++ app/assets/javascripts/init.js
@@ -2,9 +2,6 @@ var ready = function() {
   $('.button-collapse').sideNav();
   $('.parallax').parallax();
   $('.carousel').carousel();
-  // setTimeout(function(){
-  //   $('.tap-target').tapTarget('open');
-  // }, 2500);
 };
 
 $(document).ready(ready);
